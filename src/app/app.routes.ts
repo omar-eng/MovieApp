6465e123d75999152b40fb323f46c68785b88277
omar0day <elomar10234@gmail.com>
2024-01-17 09:29:14 +0200

    navbar,routingtomovielist

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { MovieListComponent } from './movie-list/movie-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:"",
+        component: MovieListComponent,
+        title:"Movie"
+    }
+];
